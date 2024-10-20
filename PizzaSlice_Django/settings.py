905cbdd53c5ec9d8b3a75860831c65202faeb889
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-10-20 17:24:50 +0000

    Updated | PizzaSlice_Django - settings.py

--- PizzaSlice_Django/settings.py
+++ PizzaSlice_Django/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-81)+c(2536f8-83q4c+t)0&rz*kx)ko!(5asm2tcv10rht2v+a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-zellyboba1-pizzaslicepp-vua3n12dxk2.ws.codeinstitute-ide.net',
                  'pizzaslice-9165a3105687.herokuapp.com'
