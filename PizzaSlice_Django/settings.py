88c0c193893c1836fbc2d147ded2bad2b9cb0fdb
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-10-28 20:56:40 +0000

    Updated | settings.py

--- PizzaSlice_Django/settings.py
+++ PizzaSlice_Django/settings.py
@@ -11,25 +11,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
-import os
 import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-81)+c(2536f8-83q4c+t)0&rz*kx)ko!(5asm2tcv10rht2v+a'
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-zellyboba1-pizzaslicepp-vua3n12dxk2.ws.codeinstitute-ide.net',
-                 'pizzaslice-9165a3105687.herokuapp.com'
+                 'pizzaslice-9165a3105687.herokuapp.com',
                 ]
                   
 
@@ -106,17 +105,18 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
         }
-    }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://ucsn9xftapm:hlqk3mu6mwKd@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/shove_tutu_femur_37233')
-}
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://ucsn9xftapm:hlqk3mu6mwKd@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/range_grant_tall_678781')
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
