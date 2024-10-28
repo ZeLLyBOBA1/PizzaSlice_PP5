1f2df15bf68916b4de10e91cfdb5a26bc7646e42
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-10-28 17:19:35 +0000

    Updated | settings.py

--- PizzaSlice_Django/settings.py
+++ PizzaSlice_Django/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '-(n+0(70exx7v@okrar4=uno3fg^edu=xuu0-7)5v_s6o4vb*h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
