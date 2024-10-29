6b0c4d9dca80cb0ca366644ff7f29a5babf93ee2
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-10-29 10:36:29 +0000

    Updated | products

--- PizzaSlice_Django/settings.py
+++ PizzaSlice_Django/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'homepage',
     'profiles',
+    'products',
 ]
 
 MIDDLEWARE = [
--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def products_list(request):
+    """ A view to return the products page """
+    return render(request, 'products/products_list.html')
