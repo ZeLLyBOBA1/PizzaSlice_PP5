709db3e4f003f95c9a57a340f63bb7e69ec26a3f
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-10-29 13:42:17 +0000

    Updated | settings.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=64)
+    sku =  models.CharField(max_length=128)
+    description = models.TextField()    
+    image = CloudinaryField('image', blank=True, null=True)
+    price = models.CharField(max_length=32)
+    category = models.CharField(max_length=64)
+
