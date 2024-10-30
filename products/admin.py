893ed9e55e49c5ed411be38e493bee2cc0848616
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-10-30 13:51:16 +0000

    Updated | products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sku', 'price',)  # Поля, которые будут отображаться в списке
+    search_fields = ('title', 'sku',)  # Поля для поиска в админке
+    list_filter = ('category',)  # Фильтрация по категориям
+
+admin.site.register(Product, ProductAdmin)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def products_list(request):
     """ A view to return the products page """
-    return render(request, 'products/products_list.html')
+    products = Product.objects.all()  # Получаем все продукты
+    return render(request, 'products/products_list.html', {'products': products})
