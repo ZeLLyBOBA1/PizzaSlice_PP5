8ce7e84e8ca0722ddd2da1093a82b1d4bc034f6a
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-11-05 14:40:24 +0000

    Updated | products - views.py

--- products/views.py
+++ products/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, Category
+from django.db.models import Q
 
 # Create your views here.
 def products_list(request):
     """ A view to return the products page """
-    sort_by = request.GET.get('sort', 'price')  
+    products = Product.objects.all() 
+    query = None 
+    categories = None
+    sort_by = None
+    
+    if request.GET:
+        if 'sort' in request.GET:
+            sort_by = request.GET.get('sort', 'price')
+            if sort_by not in ['price', '-price']:
+                sort_by = 'price'
+            products = Product.objects.all().order_by(sort_by)  
 
-    if sort_by not in ['price', '-price', 'category']:
-        sort_by = 'price' 
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-    products = Product.objects.all().order_by(sort_by)  
-    return render(request, 'products/products_list.html', {'products': products, 'sort_by': sort_by})
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                return redirect(reverse('products_list'))
+
+            queries = Q(title__icontains=query) |  Q(description__icontains=query)
+            products = products.filter(queries)
+        
+    return render(request, 'products/products_list.html', {'products': products,'search_term': query, 'current_categories': categories, 'sort_by': sort_by, })
